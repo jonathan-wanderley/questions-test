7128051f8f7e158dce0b58b3eae23d9ec8b54a3d
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-04-28 09:44:09 -0300

    feat: implement error handling w/ express

---
+++ src/errors/AppError.ts
@@ -0,0 +1,9 @@
+export class AppError {
+  statusCode: number;
+  message: string;
+
+  constructor(statusCode: number = 400, message: string) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
 import routes from "./routes";
+import { ErrorHandler } from "./middlewares/errorHandler";
 
 import "dotenv/config";
 
@@ -10,9 +12,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(ErrorHandler);
 
 app.get('/test', (req, res) => { res.json({ test: 'ok' }) })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 })
