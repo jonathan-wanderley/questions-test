fadac41fb0c9759ddbb3b9cb1b0378447eca999c
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-04-28 18:35:34 -0300

    feat: add data validation

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/AppError";
+import { ValidationError } from "express-validation";
 
 export const ErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if(err instanceof AppError) {
     return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err);
   }
 
   return res.status(500).json({
